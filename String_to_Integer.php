a55e9202e3d340e3ecbf341cb7289066a5f70943
rajibbinalam <rajibbinalam16@gmail.com>
2023-08-20 19:00:35 +0600

    update

--- String_to_Integer.php
+++ String_to_Integer.php
@@ -1,12 +1,21 @@
 <?php
             // STATUS: Working
+    const BR = '<br/>';
 class Solution {
 
     function myAtoi($s) {
-        return preg_replace('/[^0-9-]/', '', $s);
+        $trim_string = preg_replace('/[^0-9-]/', '', $s);
+        return $trim_string = ltrim($trim_string, '0');
+
+        // if($trim_string > -2147483648 && $trim_string < 2147483647-1){
+        //     return $trim_string;
+        // }
     }
 }
 
 // $s = "   _   ____-42";
-$s = "003200";
+// $s = "003200";
+// $s = "4193 with words";
+// $s = "words and 987";
+$s = "_____-+123";
 echo (new Solution())->myAtoi($s);
