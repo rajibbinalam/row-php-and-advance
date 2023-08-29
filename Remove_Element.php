025011d6930b8e003591f4299f6dccbfd10a3395
rajibbinalam <rajibbinalam16@gmail.com>
2023-08-29 14:50:16 +0600

    Remove_Element Accepted using array_filter method

--- Remove_Element.php
+++ Remove_Element.php
@@ -1,23 +1,17 @@
 <?php
-            // STATUS: working
+            // STATUS: Accepted
     const BR = '<br/>';
 class Solution {
 
     function removeElement(&$nums, $val) {
-        $new_nums = [];
-         foreach ($nums as $key => $num) {
-            if($num != $val){
-                array_push($new_nums, $num);
-            }
-        }
-        $k = sizeof($new_nums);
-        sort($new_nums);
-        print_r($new_nums);
-        return $k;
+        $nums = array_filter($nums, function($value) use($val){
+            return $value!=$val;
+        });
+        print_r($nums);
+        echo sizeof($nums);
+        return sizeof($nums);
     }
 }
-// $nums = [3,2,2,3];
-// $val = 3;
-$nums = [0,1,2,2,3,0,4,2];
-$val = 2;
+// $nums = [3,2,2,3]; $val = 3;
+$nums = [0,1,2,2,3,0,4,2]; $val = 2;
 echo (new Solution())->removeElement($nums, $val);
