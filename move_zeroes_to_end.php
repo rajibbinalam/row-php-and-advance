6c38b9f413eb675c4dc1c5f1118b65711acf15e8
rajibbinalam <rajibbinalam16@gmail.com>
2023-08-29 15:17:33 +0600

    move zeroes to the end

---
+++ move_zeroes_to_end.php
@@ -0,0 +1,24 @@
+<?php
+            // STATUS: Accepted
+    const BR = '<br/>';
+class Solution {
+
+    function moveZeroes(&$nums) {
+        $count=0;
+        $nums = array_filter($nums, function($value) use(&$count){
+            if($value==0){
+                $count++;
+            }
+            return $value!=0;
+        });
+        while($count != 0){
+            array_push($nums, 0);
+            $count--;
+        }
+        return $nums;
+    }
+}
+// $nums = [3,0,2,3];
+$nums = [0,1,0,3,12];
+print_r((new Solution())->moveZeroes($nums));
+// print_r((new Solution())->moveZeroes($nums));
