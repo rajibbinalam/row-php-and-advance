c6ac59cb994dc84531db3d986be0ee3e5befd0a0
rajibbinalam <rajibbinalam16@gmail.com>
2023-08-29 19:10:53 +0600

    working on Merge_Two_Sorted_Lists

---
+++ Merge_Two_Sorted_Lists.php
@@ -0,0 +1,13 @@
+<?php
+            // STATUS: Working
+class Solution {
+
+    function mergeTwoLists($list1, $list2) {
+
+        sort($new);
+        return $new;
+    }
+}
+$list1 = [1,2,4]; $list2 = [1,3,4];
+print_r((new Solution())->mergeTwoLists($list1, $list2));
+// print_r((new Solution())->mergeTwoLists($list1, $list2));
