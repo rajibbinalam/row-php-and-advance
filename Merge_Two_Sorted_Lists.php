b57ac16236ced3bb437ba446c83c7087d71b0494
rajibbinalam <rajibbinalam16@gmail.com>
2023-08-30 16:17:45 +0600

    u

--- Merge_Two_Sorted_Lists.php
+++ Merge_Two_Sorted_Lists.php
@@ -3,11 +3,22 @@
 class Solution {
 
     function mergeTwoLists($list1, $list2) {
-
+        $new = array_merge($list1, $list2);
         sort($new);
         return $new;
     }
 }
+
+class Node {
+    public $data;
+    public $next;
+
+    public function __construct($data) {
+        $this->data = $data;
+        $this->next = null;
+    }
+}
+
 $list1 = [1,2,4]; $list2 = [1,3,4];
 print_r((new Solution())->mergeTwoLists($list1, $list2));
 // print_r((new Solution())->mergeTwoLists($list1, $list2));
