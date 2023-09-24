f5b9b014dbdccec8488a97dd9a8a3599a8b540c1
rajibbinalam <rajibbinalam16@gmail.com>
2023-09-24 18:10:18 +0600

    up

--- leetcode/Find_and_Replace_Pattern.php
+++ leetcode/Find_and_Replace_Pattern.php
@@ -4,19 +4,27 @@
 class Solution {
 
     function findAndReplacePattern($words, $pattern) {
-        $n_arr = [];
-        foreach ($words as $key => $word) {
-            if(strlen($word) != strlen($pattern)){
-                continue;
-            }
-            $x_words = str_split($word);
-            $x_patterns = str_split($pattern);
-            foreach ($x_words as $key => $x_word) {
-                $x_patterns[$key] = $x_words[$key];
-            }
-
-            print_r($x_patterns);
+        $x_patterns = str_split($pattern);
+        $c = 0;
+        foreach ($x_patterns as $key => $x_pattern) {
+            $$x_pattern = ++$c;
         }
+        print_r($x_patterns->$$x_pattern);
+
+
+        // $n_arr = [];
+        // foreach ($words as $key => $word) {
+        //     if(strlen($word) != strlen($pattern)){
+        //         continue;
+        //     }
+        //     $x_words = str_split($word);
+
+        //     foreach ($x_words as $key => $x_word) {
+        //         $x_patterns[$key] = $x_words[$key];
+        //     }
+
+        //     print_r($x_patterns);
+        // }
 
     }
 }
