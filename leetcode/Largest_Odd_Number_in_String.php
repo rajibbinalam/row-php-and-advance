0164c37b15bfbeaf4ac2de28df6604ed0107570f
rajibbinalam <rajibbinalam16@gmail.com>
2023-10-03 10:50:51 +0600

    no changes

--- leetcode/Largest_Odd_Number_in_String.php
+++ leetcode/Largest_Odd_Number_in_String.php
@@ -7,21 +7,22 @@ class Solution {
         $numArr = str_split($num);
 
         for ($i=count($numArr); $i > 0; $i--) { 
-            $nNum = implode('', $numArr) ?? [];
+            $nNum = implode('', $numArr ?? []);
+            $nNum = trim(+$nNum);
             if($nNum%2 != 0){
                 return $nNum;
             }else if($nNum%2 == 0){
                 array_pop($numArr);
             }else{
-                return "";
+                return false;
             }
         }
         // return $nNum ?? "";
     }
 }
 // $s = "2345";
-$s = "52";
-// $s = "4206";
+// $s = "52";
+$s = "4206";
 // $s = "35427";
 // $s = "10133890";
 echo (new Solution())->largestOddNumber($s);
